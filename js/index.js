4b7d6053fcdc9f4ea7597b662706396e66d3a13d
Gapilan <gapilan.nicholas@gmail.com>
2022-07-27 23:21:46 -0400

    Updated HTML, CSS, and JS code

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("Your index.js file is loaded correctly!");
+
+document.getElementById("resumeButton").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("You're about to go to my awesome Resume");
+}
